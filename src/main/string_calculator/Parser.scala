f83e7817137f750dead0731615ae27fc0786e926
Alfredo Artiles <aartiles@gmail.com>
2019-10-01 18:38:07 +0200

    parser take into account a non default delimiter

--- src/main/string_calculator/Parser.scala
+++ src/main/string_calculator/Parser.scala
@@ -18,44 +18,75 @@ object Parser {
 }
 
 final class Parser(val input: String, val delimiter: String = null) {
-  val DELIMITERS = Array('\n', ',')
+  private val DELIMITERS = Array("\n", "","")
+  private var position = 0
 
   def parse: Array[Double] = {
+    var nextType = "number"
     var numbers = Array.empty[Double]
-    var position = 0
+
     do {
-      val (currentPosition, token) = this.nextToken(position)
-      if (token.isEmpty) {
-        position = currentPosition - 1
-        val c = this.input.charAt(position)
-        throw new RuntimeException(s"Number expected but '$c' found at position $position.")
+      val prevPosition = this.position
+      val (token, tokenType) = this.nextToken
+      println("token", token, tokenType, this.position)
+      println("nextType before", nextType)
+
+      if (nextType == "number" && tokenType == "number") {
+        numbers = numbers :+ token.toDouble
+        nextType = "delimiter"
+      }
+      else if (nextType == "number" && tokenType == "delimiter") {
+        throw new RuntimeException(s"Number expected but '$token' found at position $prevPosition.")
+      }
+      else if (nextType == "delimiter" && tokenType == "delimiter" && this.position > this.input.length - 1) {
+        throw new RuntimeException("Number expected but EOF found")
+      }
+      else {
+        nextType = "number"
       }
 
-      numbers = numbers :+ token.toDouble
-      position = currentPosition
     }
-    while (position < input.length)
+    while (this.position < input.length)
+    println("nextType after", nextType)
+    println("numbers size", numbers.length)
     numbers
   }
 
-  private def nextToken(initialPosition: Int): (Int, String) = {
-    var currentPosition = initialPosition
-    var number = ""
+  private def nextToken: (String, String) = {
+    var token = ""
+    var tokenType = ""
+
     do {
-      val c = this.input.charAt(currentPosition)
-      if (this.isDelimiter(c) && currentPosition == this.input.length - 1) {
-        throw new RuntimeException("Number expected but EOF found")
+      val c = this.input.charAt(this.position)
+      if ((tokenType == "number" || tokenType.isEmpty) && this.isNumeric(c)) {
+        token += c
+        tokenType = "number"
       }
-      if (this.isDelimiter(c)) return (currentPosition + 1, number)
-      number += c
-      currentPosition += 1
+      else if (tokenType.isEmpty) {
+        token += c
+        tokenType = "delimiter"
+      }
+      else if (this.isDelimiter(token + c)) {
+        token += c
+        tokenType = "delimiter"
+        return (token, tokenType)
+      }
+      else {
+        return (token, tokenType)
+      }
+      this.position += 1
     }
-    while (currentPosition < this.input.length)
-    (currentPosition, number)
+    while (this.position < this.input.length)
+    return (token, tokenType)
   }
 
-  private def isDelimiter(c: Char): Boolean = {
-    if (c.toString == this.delimiter) true else DELIMITERS contains c
+  private def isDelimiter(token: String): Boolean = {
+    if (token.toString == this.delimiter)
+      true
+    else
+      this.DELIMITERS contains token
   }
+
+  private def isNumeric(c: Char): Boolean = "0123456789.".contains(c)
 
 }
